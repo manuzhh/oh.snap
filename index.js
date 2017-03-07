b913b24772ffb7544b39a913e874ad0af8db13bf
jakob <jakob.blobel@haw-hamburg.de>
2017-03-07 13:09:29 +0100

    More Doku.

--- index.js
+++ index.js
@@ -62,40 +62,106 @@ let runFunctionList = (obj, functions, options=[]) => {
 	return obj
 }
 
-
+/**
+ * Registriert eine neue Reducer-Funktion für User-Objekte. Diese kann dann von allen User-Funktionen, User-Objekte schreiben verwendet werden indem der identifer in den jeweiligen reducers-Parameter übernommen wird.
+ * @param  {String} identifer Name der Funktion für spätere Verwendung. Darf nicht "default" sein.
+ * @param  {Function} func    Funktion die ein User-Objekt als Parameter akzeptiert und dieses verändert wieder zurückgibt.
+ */
 let userReducersRegister = (identifer, func) => {
 	if (identifer instanceof String && func instanceof Function) {
 		userReducers[identifer] = func
 	}
 }
 
+/**
+ * Liefert eine Liste mit den Identifern aller registrierten Reducer-Funktion für User-Objekte.
+ * @return {Array} Liste der Identifer.
+ */
 let usersReducersKeys =  () => {
 	return Object.keys(userReducers);
 }
 
+/**
+ * Registriert eine neue Builder-Funktion für User-Objekte. Diese kann dann von allen User-Funktionen, User-Objekte lesen verwendet werden indem der identifer in den jeweiligen builders-Parameter übernommen wird.
+ * @param  {String} identifer Name der Funktion für spätere Verwendung. Darf nicht "default" sein.
+ * @param  {Function} func    Funktion die ein User-Objekt als Parameter akzeptiert und dieses verändert wieder zurückgibt.
+ */
 let usersBuildersRegister = (identifer, func) => {
 	if (identifer instanceof String && func instanceof Function) {
 		userBuilders[identifer] = func
 	}
 }
 
+/**
+ * Liefert eine Liste mit den Identifern aller registrierten Builder-Funktion für User-Objekte.
+ * @return {Array} Liste der Identifer.
+ */
 let usersBuildersKeys = () => {
 	return Object.keys(userBuilders);
 }
 
+/**
+ * Login-Funktion für User. Erwartet ein Objekt mit den Logindaten und eine callback-Funktion, die aufgerufen wird sobald der Loginvorgang abgeschlossen wurde.
+ * Diese Funktion wird über users.login in die API eingebunden.
+ * @param  {Object}   loginData Objekt welches die Elemente userName und password beinhaltet.
+ * @param  {Function} callback  Eine Funktion die Aufgerufen werden soll, sobald der Login-Prozess abgeschlossen wurde. Diese erhält zwei Parameter (error, response).
+ * Der erste Parameter enthält einen Errorcode, falls der Login fehlgeschlagen ist, ansonsten null.
+ * Der zweite Parameter enthält das User-Objekt für den Fall, dass der Login erfolgreich war, ansonsten null.
+ */
 let usersLogin = (loginData, callback) => {
 	userModule.login(loginData, callback)
 }
 
+/**
+ * Funktion die einen neuen User registriert. Dazu wird ein User-Objekt übegerben, welches alle Daten des neu anzulegenden Users enthält, eine Liste von Builder-Funktion um
+ * dem User-Objekt weitere Eigenschaften hinzuzufügen, sowie eine callback-Funktion die aufgerufen wird sobald der Vorgan abgeschlossen ist.
+ * Das übergebene User-Objekt muss die Elemente userName, password und passwordConf enthalten. der Inhalt der von password und passwordConf muss identisch sein.
+ * Das übergbene User-Objekt darf keine Elemente enhalten mit dem Bezeichnern:
+ * 	hashedPassword
+ * 	online
+ * 	followl
+ * 	followedl
+ * 	content
+ *
+ * Diese Funktion wird über users.register in die API eingebunden.
+ *
+ * @param  {Object}   newUserObj             Neues User-Objekt mit den oben beschriebenen Eigenschaften.
+ * @param  {Array}    [builders=["default"]] Liste mit Bezeichnern von Builder-Funktion, die auf das User-Objekt angewendet werden sollen.
+ * @param  {Function} callback               Eine Funktion die Aufgerufen werden soll, sobald der Registrierungs-Prozess abgeschlossen wurde. Diese erhält zwei Parameter (error, response).
+ * Der erste Parameter enthält einen Errorcode, falls die Registrierung fehlgeschlagen ist, ansonsten null.
+ * Der zweite Parameter enthält das User-Objekt für den Fall, dass die Registrierung erfolgreich war, ansonsten null.
+ */
 let usersRegister = (newUserObj, builders = ["default"], callback) => {
 	newUserObj = runFunctionList(newUserObj, userBuilders, builders)
 	userModule.register(newUserObj, callback)
 }
 
+/**
+ * Funktion zum authetifizieren eines Users.
+ * Dazu wird der Funktion das JSON-Webtoken des Users übergeben. Ist das Token valide wird das User-Objekt des Users zurückgegeben.
+ *
+ * Diese Funktion wird über users.authenticate in die API eingebunden.
+ * @param  {String} authToken              JSON-Webtoken des zu authetifizierenden Users.
+ * @param  {Array}  [reducers=["default"]] Liste mit Bezeichnern von Reducer-Funktion, die auf das User-Objekt angewendet werden sollen bevor es zurückgegeben wird.
+ * @return {Object}                        User-Objekt des zu authetifizierenden benutzers oder null.
+ */
 let usersAuthenticate = (authToken, reducers = ["default"]) => {
 	return userModule.authenticate(authToken)
 }
 
+/**
+ * Funktion zum Abfragen von User-Objekten.
+ *
+ * Diese Funktion wird über users.get in die API eingebunden.
+ * @param  {String}   authToken              JSON-Webtoken des anfragenden Users.
+ * @param  {Object}   options                Stellt den Filter da, der die Menge der zu ermittelnden User spezifiziert.
+ * z. B. könnte das Objekt folgendermaßen aussehen: {online: true}. In diesem Fall würden
+ * nur User ermittelt, die derzeit auf eingeloggt sind.
+ * @param  {Array}    [reducers=["default"]] Liste mit Bezeichnern von Reducer-Funktion, die auf die User-Objekte angewendet werden sollen bevor sie zurückgegeben wird.
+ * @param  {Function} callback               Eine Funktion die Aufgerufen werden soll, sobald der Such-Prozess abgeschlossen wurde. Diese erhält zwei Parameter (error, response).
+ * Der erste Parameter enthält einen Errorcode, falls die Suche fehlgeschlagen ist, ansonsten null.
+ * Der zweite Parameter enthält ein Array von User-Objekten oder null.
+ */
 let usersGet = (authToken, options, reducers = ["default"], callback) => {
 	userModule.getUsers({"token": authToken}, options, true, (err, users) => {
 		if (!err && users) {
